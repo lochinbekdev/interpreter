8f6ee8d1dcdde8a7b27206c326ebde6f428e78d7
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2025-07-21 09:07:31 +0500

    add new conditions

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -66,6 +66,21 @@ public class Scanner {
             case '*':
                 addToken(STAR);
                 break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
         }
     }
 
@@ -82,6 +97,15 @@ public class Scanner {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
 
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd())
+            return false;
+        if (source.charAt(current) != expected)
+            return false;
+        current++;
+        return true;
     }
 
 }
